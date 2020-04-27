4bd6b2b20471071bcedd1292b3fd50925201b555
Tom Chen <tom@itri.org.tw>
2020-04-27 21:27:28 +0800

    chore: use commonjs module

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('hello, serverjs2 ?');
+console.log('hello, CommonJS module');
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+console.log(__dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000);
