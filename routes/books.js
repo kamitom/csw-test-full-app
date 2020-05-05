838f8cc017cf1ea940e1d0e431a6b4bb2ea2c218
Tom Chen <tom@itri.org.tw>
2020-05-05 12:33:44 +0800

    feat: new book route

--- routes/books.js
+++ routes/books.js
@@ -3,15 +3,31 @@ const router = express.Router();
 
 const fakerDep = require('../tools/persion');
 const Book = require('../model/book');
+const Author = require('../model/author');
 
 // All Books Route
 router.get('/', async (req, res) => {
-  res.send('All Books');
+  // res.send('All Books');
+
+  res.render('books/index');
 });
 
 // New Book Route
-router.get('/new', (req, res) => {
-  res.send('New Book2');
+router.get('/new', async (req, res) => {
+  // res.send('New Book2');
+
+  try {
+    const authorsFound = await Author.find({});
+    const aNewBook = new Book();
+    res.render('books/new', {
+      allAuthors: authorsFound,
+      theBook: aNewBook,
+    });
+  } catch (error) {
+    res.redirect('books');
+  }
+
+  // res.render('books/new', { book: new Book()})
 });
 
 // Create Book Route
